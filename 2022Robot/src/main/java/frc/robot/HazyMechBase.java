65e6fe6961b4c87946ff25e759b9c333bc059841
JasonRen003 <Jasren003@gmail.com>
2022-01-17 10:43:47 -0600

    ?
    ??

--- 2022Robot/src/main/java/frc/robot/HazyMechBase.java
+++ 2022Robot/src/main/java/frc/robot/HazyMechBase.java
@@ -1,7 +1,6 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
-import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
@@ -20,23 +19,47 @@ public class HazyMechBase extends Subsystem{
 
 
     public void driveCartesian(double x, double y, double angle){
-        y = MathUtil.clamp(y, -1.0,1.0);
-        x = MathUtil.clamp(x, -1.0,1.0);
+        y = clamp(y, -1.0,1.0);
+        x = clamp(x, -1.0,1.0);
 
         double[] wheelSpeeds = new double[4];
         wheelSpeeds[0] = x + y + angle;
         wheelSpeeds[1] = -x + y - angle;
         wheelSpeeds[2] = -x + y + angle;
         wheelSpeeds[3] = x + y - angle;
-    
+
         normalize(wheelSpeeds);
-    
-        lFrontSpark.set(ControlMode.PercentOutput, -wheelSpeeds[0] );
-        rFrontSpark.set(ControlMode.PercentOutput, -wheelSpeeds[1] * -1);
-        lBackSpark.set(ControlMode.PercentOutput, -wheelSpeeds[2]);
-        rBackSpark.set(ControlMode.PercentOutput, -wheelSpeeds[3]*-1);
+
+        lFrontSpark.set(-wheelSpeeds[0] );
+        rFrontSpark.set(-wheelSpeeds[1] * -1);
+        lBackSpark.set(-wheelSpeeds[2]);
+        rBackSpark.set(-wheelSpeeds[3]*-1);
     }
-    
+
+    protected void normalize(double[] wheelSpeeds) {
+        double maxMagnitude = Math.abs(wheelSpeeds[0]);
+
+        for (int i = 1; i < wheelSpeeds.length; i++) {
+            double temp = Math.abs(wheelSpeeds[i]);
+            if (maxMagnitude < temp) 
+                maxMagnitude = temp;
+        }
+
+        if (maxMagnitude > 1.0) {
+            for (int i = 0; i < wheelSpeeds.length; i++) {
+                wheelSpeeds[i] = wheelSpeeds[i] / maxMagnitude;
+            }
+        }
+    }
+
+    private double clamp(double input, double low, double high){
+        if(input > high)
+            return high;
+        else if(input < low)
+            return low;
+        return input;
+
+    }
     @Override
     public void initDefaultCommand(){
         driveCartesian(0,0,0);
