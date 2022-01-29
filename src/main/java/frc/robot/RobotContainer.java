57812e933816fa5da3d5444c2c2c40f001d61436
JasonRen003 <56003847+JasonRen003@users.noreply.github.com>
2022-01-29 11:24:44 -0600

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,17 +3,22 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Joystick;
 
 public class RobotContainer {
+    
+    //Robot's controllers that drivers use
     Joystick leftJoystick = new Joystick(RobotMap.LEFTJOYSTICKPORT);
     Joystick rightJoystick = new Joystick(RobotMap.RIGHTJOYSTICKPORT);
     
+    //Chassis subystem and command setup
     HazyMechBase hazyMechBase = new HazyMechBase();
     CommandMecanum commandMecanum = new CommandMecanum(hazyMechBase, leftJoystick, rightJoystick);
-
+    
+    //This constructor is called once in Robotinit and should set up all button-> command bindings and default commands
     public RobotContainer(){
         configureButtonBindings();
         hazyMechBase.setDefaultCommand(commandMecanum);
     }
-
+    
+    //Use this method to define button->command mappings
     public void configureButtonBindings () {
 
     }
